829d02e5be4d0bf437ccb4d998bbee01caf53cde
Tejasi <gaurtejasi05@gmail.com>
2026-04-16 02:57:32 +0530

    remove folders from repo

--- rise4/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-   let tempC=$state(0)
-   let tempF=$derived((tempC*9/5)+32)
-
-</script>
-<label>Temp in C</label>
-<input type="number" bind:value={tempC}><br>
-<label>Temp in F</label>
-<h1>{tempF}</h1>
